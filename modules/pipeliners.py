235b043bcd75a7572e6461fc98bdcd437c0c368a
Kim Brugger <kbr@brugger.dk>
2015-08-21 09:59:41 +0100

    test using drmaa for sge backend...

--- modules/pipeliners.py
+++ modules/pipeliners.py
@@ -1,6 +1,8 @@
 
 
 import subprocess
+import sge
+
 
 __verbose_level = 0
 
@@ -19,6 +21,17 @@ REV_VERBOSE_LEVELS = { 0 : 'DEBUG',
 
 __steps = list();
 
+__USE_SGE = 0
+
+def local():
+    global __USE_SGE
+    __USE_SGE = 0
+
+def SGE():
+    global __USE_SGE
+    __USE_SGE = 1
+
+
 def set_verbose_level( new_level ):
 
     global __verbose_level
@@ -30,9 +43,7 @@ def set_verbose_level( new_level ):
         print "New verbose level: " + REV_VERBOSE_LEVELS[ __verbose_level ]
     else:
         print "Unknown verbosity level: " + new_level
-
-
-    
+   
 
 def verbose_print( message, level ):
 
@@ -57,9 +68,58 @@ def run_steps():
 #------------------------------------------------------------
 # Make system call fucntion that checks if the function exited correctly
 
-def system_call( step_name, cmd ):
+def system_call( step_name, *cmds ):
 
     verbose_print(cmd, 'DEBUG')
+
+    if ( __USE_SGE ):
+        _run_on_SGE( step_name, *cmds )
+
+    else:
+        _run_local(step_name, *cmds)
+
+
+def _run_on_SGE( step_name, cmd):
+    import drmaa
+    import os
+
+    command = cmd.split(" ")
+#    print cmd
+#    print " - ".join( command )
+
+#    print command[0] + command[1]
+
+    with drmaa.Session() as s:
+#        print('Creating job template')
+        jt = s.createJobTemplate()
+#        jt.remoteCommand = os.path.join(os.getcwd(), command[0])
+        jt.remoteCommand = os.path.join(command[0])
+        jt.args = command[1:])
+#        jt.args = ['5']
+        jt.joinFiles=True
+                   
+        jobid = s.runJob(jt)
+        print('Your job has been submitted with ID %s' % jobid)
+
+        retval = s.wait(jobid, drmaa.Session.TIMEOUT_WAIT_FOREVER)
+
+        print('Job: {0} finished with status {1}'.format(retval.jobId, retval.hasExited))
+        import pprint as pp
+        pp.pprint( retval )
+
+        if ( retval.exitStatus == 0):
+            verbose_print("Script failed at %s stage, jobid: %s" % (step_name, jobid), 'DEBUG')
+            verbose_print("Script failed at %s stage" % (step_name), 'INFO')
+            exit()
+        else:
+            verbose_print("Script sucessful at %s stage" % (step_name), 'INFO')
+            verbose_print("Job %s (jobid: %s) successful !" % (step_name, jobid), 'DEBUG')
+            s.deleteJobTemplate(jt)
+    
+
+
+def _run_local( step_name, cmd ):
+
 
     try:
         subprocess.check_call(cmd, shell=True)
